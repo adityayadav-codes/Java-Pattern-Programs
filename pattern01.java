c7def7e48ba2cc47db1f7d5537e4ef0e87f7df52
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-01 23:25:11 +0530

    pattern 5cd

--- pattern01.java
+++ pattern01.java
@@ -97,19 +97,40 @@
 5 4 3 2 1
  */
 
- public class pattern01 {
+//  public class pattern01 {
 	
-	public static void main(String args[])		{
+// 	public static void main(String args[])		{
 		
-		for (int i = 5; i >=1; i--)	{
+// 		for (int i = 5; i >=1; i--)	{
 			
-			for(int j = 5; j >=1; j--)	{
+// 			for(int j = 5; j >=1; j--)	{
 				
-				System.out.print(j + " ");
+// 				System.out.print(j + " ");
+// 			}
+// 			System.out.println();
+// 		}
+//  	}
+// }
+
+/*
+ pattern 6:
+1 2 3 4 5
+6 7 8 9 10
+11 12 13 14 15
+16 17 18 19 20
+21 22 23 24 25
+*/
+public class pattern01 {
+		
+	public static void main (String args [])	{
+		
+		for (int i = 1; i <=25; i++)	{
+			
+			for (int j = 1; j <= 5; j++)	{
+				
+				System.out.print(i + " ");
 			}
 			System.out.println();
 		}
- 	}
+	}
 }
-
-
