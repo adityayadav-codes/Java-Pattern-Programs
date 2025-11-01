c152ce069144bd44cc07ce8172e443aacb791769
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-01 23:32:27 +0530

    patern 6

--- pattern01.java
+++ pattern01.java
@@ -120,17 +120,20 @@
 16 17 18 19 20
 21 22 23 24 25
 */
-public class pattern01 {
+
+ public class pattern01 {
+	
+	public static void main(String args[])		{
 		
-	public static void main (String args [])	{
-		
-		for (int i = 1; i <=25; i++)	{
+		int count = 1;
+		for (int i = 1; i <=5; i++)	{
 			
-			for (int j = 1; j <= 5; j++)	{
+			for(int j = 1; j <=5; j++)	{
 				
-				System.out.print(i + " ");
+				System.out.print(count + " ");
+				count++;
 			}
 			System.out.println();
 		}
-	}
+  	}
 }
