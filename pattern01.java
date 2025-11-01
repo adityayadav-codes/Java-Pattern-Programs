cdd3779814601f49300ca40eeda0a2130832f2f4
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-01 23:43:02 +0530

    patern 7

--- pattern01.java
+++ pattern01.java
@@ -121,19 +121,45 @@
 21 22 23 24 25
 */
 
- public class pattern01 {
+//  public class pattern01 {
 	
-	public static void main(String args[])		{
+// 	public static void main(String args[])		{
 		
-		int count = 1;
-		for (int i = 1; i <=5; i++)	{
+// 		int count = 1;
+// 		for (int i = 1; i <=5; i++)	{
 			
-			for(int j = 1; j <=5; j++)	{
+// 			for(int j = 1; j <=5; j++)	{
 				
+// 				System.out.print(count + " ");
+// 				count++;
+// 			}
+// 			System.out.println();
+// 		}
+//   	}
+// }
+
+/*
+ pattern 7:
+1 3 5 7 9
+2 4 6 8 10
+11 13 15 17 19
+21 23 25 27 29
+31 33 35 37 39
+41 43 45 47 49
+*/
+public class pattern01 {
+	public static void main(String args [])	{
+		
+		int count =1;
+		
+		for(int i =1; i<=5; i++)	{
+		
+			for(int j = 1; j <=5; j++)	{
+			
 				System.out.print(count + " ");
-				count++;
+				count +=2;
 			}
 			System.out.println();
 		}
-  	}
+	}
 }
