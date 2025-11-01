3623ddeed3a819805784821d5a520730c01884c0
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-01 23:58:04 +0530

    pattern 9

--- pattern01.java
+++ pattern01.java
@@ -173,19 +173,43 @@
  42 44 46 48 50
 */
 
-public class pattern01 {	
+// public class pattern01 {	
 	
-	public static void main (String args [] )	{
+// 	public static void main (String args [] )	{
 		
-		int count =2;
-			for (int i = 1; i <=5; i++)	{
+// 		int count =2;
+// 			for (int i = 1; i <=5; i++)	{
 		
-				for(int j = 1; j <=5;j++)	{
+// 				for(int j = 1; j <=5;j++)	{
 				
-					System.out.print(count + " ");
-					count +=2;
+// 					System.out.print(count + " ");
+// 					count +=2;
+// 			}
+// 				System.out.println();
+// 		}			
+// 	}
+// }
+
+/*
+ pattern 9:
+ 1 2 3 4 5
+ 2 4 6 8 10
+ 3 6 9 12 15
+ 4 8 12 16 20
+ 5 10 15 20 25
+ */
+ 
+ public class pattern01	{
+	
+	public static void main(String args [])	{
+		
+		for (int I =1; I <=5; I++)	{
+			
+			for(int j =1; j <=5; j++)	{
+		
+					System.out.print(I * j + " ");
 			}
 				System.out.println();
-		}			
+		}
 	}
 }
