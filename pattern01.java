61f66ec1c28605dcecb9212d12ebb42ef05e35c0
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-01 23:50:30 +0530

    pattern 8

--- pattern01.java
+++ pattern01.java
@@ -147,19 +147,45 @@
 31 33 35 37 39
 41 43 45 47 49
 */
-public class pattern01 {
-	public static void main(String args [])	{
+// public class pattern01 {
+// 	public static void main(String args [])	{
 		
-		int count =1;
+// 		int count =1;
 		
-		for(int i =1; i<=5; i++)	{
+// 		for(int i =1; i<=5; i++)	{
 		
-			for(int j = 1; j <=5; j++)	{
+// 			for(int j = 1; j <=5; j++)	{
 			
-				System.out.print(count + " ");
-				count +=2;
+// 				System.out.print(count + " ");
+// 				count +=2;
+// 			}
+// 			System.out.println();
+// 		}
+// 	}
+// }
+
+/*
+ pattern 8:
+ 2 4 6 8 10
+ 12 14 16 18 20
+ 22 24 26 28 30
+ 32 34 36 38 40
+ 42 44 46 48 50
+*/
+
+public class pattern01 {	
+	
+	public static void main (String args [] )	{
+		
+		int count =2;
+			for (int i = 1; i <=5; i++)	{
+		
+				for(int j = 1; j <=5;j++)	{
+				
+					System.out.print(count + " ");
+					count +=2;
 			}
-			System.out.println();
-		}
+				System.out.println();
+		}			
 	}
 }
