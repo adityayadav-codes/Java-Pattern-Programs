f2ab478b86e29b070e8d89722754d3cb9efe1744
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-02 12:21:40 +0530

    patter 12

--- pattern02.java
+++ pattern02.java
@@ -7,17 +7,45 @@
  5 1 5 2 5 3
  */
 
- public class pattern02 {
+//  public class pattern02 {
 	
+// 	public static void main (String args [])	{
+		
+// 		for(int i =1; i <= 5; i++)	{
+			
+// 			for (int j = 1; j <= 3; j++)	{
+				
+// 				System.out.print(i + " " + j +" " );
+// 			}
+// 			System.out.println();
+// 		}
+// 	}
+// }
+
+/*
+    pattern 12:
+    1 6 11 16 21
+    2 7 12 17 22
+    3 8 13 18 23
+    4 9 14 19 24
+    5 10 15 20 25
+ */
+
+ public class pattern02 {
+		
 	public static void main (String args [])	{
 		
+		int count;
 		for(int i =1; i <= 5; i++)	{
+            count = i;
 			
-			for (int j = 1; j <= 3; j++)	{
+			for(int j =1; j <= 5; j++)	{
 				
-				System.out.print(i + " " + j +" " );
+				System.out.print(count + " ");
+					count +=5; 
 			}
-			System.out.println();
+				System.out.println();
 		}
 	}
 }
+
