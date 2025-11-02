b3e5124df36fba93960b7c07e2bd2cb79aaa9d07
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-02 13:03:57 +0530

    pattern 14

--- pattern02.java
+++ pattern02.java
@@ -58,29 +58,55 @@
     5 6 15 16 25
  */
     
-  public class pattern02 {
+//   public class pattern02 {
         
-        public static void main (String args []){
+//         public static void main (String args []){
             
-            int count1, count2;
-                for(int i =1; i <=5; i++) {
+//             int count1, count2;
+//                 for(int i =1; i <=5; i++) {
                     
-                    count1 = i;
-                    count2 = 5 - i + 1;
-                    for(int j = 1; j <= 5; j++) {
+//                     count1 = i;
+//                     count2 = 5 - i + 1;
+//                     for(int j = 1; j <= 5; j++) {
                     
-                        if(j % 2 ==1) {
+//                         if(j % 2 ==1) {
                         
-                            System.out.print(count1 + " ");
-                        } else {
+//                             System.out.print(count1 + " ");
+//                         } else {
                         
-                        System.out.print(count2   + " ");                
-                        }
-                    count1 +=5;
-                    count2 +=5;
-            }
-            System.out.println();
+//                         System.out.print(count2   + " ");                
+//                         }
+//                     count1 +=5;
+//                     count2 +=5;
+//             }
+//             System.out.println();
 
-        }
-    }
+//         }
+//     }
+// }
+/*
+    pattern 14:
+   5 10 15 20 25
+   4 9 14 19 24
+   3 8 13 18 23
+   2 7 12 17 22
+   1 6 11 16 21
+ */
+    
+ public class pattern02 {
+	
+	public static void main(String args [])	{
+		
+		int count;
+			for(int i = 5; i >= 1; i--)	{
+			
+				count =i;
+					for(int j = 5; j >= 1; j--)	{
+						
+						System.out.print(count + " ");
+					count +=5;
+			}
+			System.out.println();
+		}
+	}
 }
