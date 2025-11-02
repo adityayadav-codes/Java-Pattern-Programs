4aa400fd9b8370e51b890790511dc90d96b0c9c4
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-02 21:56:45 +0530

    pattern 19

--- pattern02.java
+++ pattern02.java
@@ -207,17 +207,41 @@
     0 1 0 1 0
  */
 
- public class pattern02	{
+//  public class pattern02	{
 		
-	public static void main (String args [  ])	{
+// 	public static void main (String args [  ])	{
+		
+// 		for (int i =1; i <= 5; i++)	{
+			
+// 			for (int j =1; j <= 5; j++){
+			
+// 				System.out.print(((i+j)%2) + " " );
+// 			}
+// 				System.out.println();
+// 		}
+// 	}
+// }
+
+/*
+    pattern 19:
+    1 0 1 0 1
+    0 1 0 1 0
+    1 0 1 0 1 
+    0 1 0 1 0
+    1 0 1 0 1
+ */
+
+ public class pattern02	 {
+	public static void main (String args [ ]) {
 		
 		for (int i =1; i <= 5; i++)	{
-			
-			for (int j =1; j <= 5; j++){
-			
-				System.out.print(((i+j)%2) + " " );
+		
+			for (int j = 0; j < 5; j++)	{
+		
+				System.out.print((i + j)%2 + " ");	
 			}
 				System.out.println();
 		}
 	}
 }
+ 
