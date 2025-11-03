574b6fdb7629cfce7d590e32dd27ed82c74f3fbd
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-03 22:16:48 +0530

    pattern 26

--- pattern03.java
+++ pattern03.java
@@ -102,17 +102,43 @@
     1 0 1 0 1
  */
 
-public class pattern03 {
+// public class pattern03 {
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
         
-        for(int i =1; i <= 5; i++) {
+//         for(int i =1; i <= 5; i++) {
             
-            for(int j = 1; j <= 5; j++){
+//             for(int j = 1; j <= 5; j++){
 
-                System.out.print(j%2 + " ");
-            }
-            System.out.println();
-        }       
+//                 System.out.print(j%2 + " ");
+//             }
+//             System.out.println();
+//         }       
+//     }
+// }
+
+/*
+    pattern 26:
+    A A A A A
+    B B B B B 
+    C C C C C
+    D D D D D
+    E E E E E
+ */
+    public class pattern03 {
+    
+        public static void main(String[] args) {
+            
+            char r ; 
+            char q ;
+
+                for(r= 'A'; r<= 'E'; r++) {
+
+                   for(q='A'; q <= 'E'; q++) {
+
+                        System.out.print(r + " ");
+                   }
+                   System.out.println();
+                }
+        }
     }
-}
