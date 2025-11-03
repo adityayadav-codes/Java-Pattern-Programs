589b4b8c8ba35bc290754d621dca105ea962622e
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-03 22:01:52 +0530

    pattern 24

--- pattern03.java
+++ pattern03.java
@@ -55,19 +55,41 @@
     1 1 1 1 1 
  */
     
- public class pattern03 {
+//  public class pattern03 {
 	
-	public static void main(String args [ ]) {
+// 	public static void main(String args [ ]) {
 			
-		for(int I = 1; I <= 5; I++)	 {
+// 		for(int I = 1; I <= 5; I++)	 {
 			
-			for(int j = 1; j <= 5; j++) {
+// 			for(int j = 1; j <= 5; j++) {
 			
-				System.out.print((I% 2) + " ");
-			}
-				System.out.println();
-		}
-	}
-}
+// 				System.out.print((I% 2) + " ");
+// 			}
+// 				System.out.println();
+// 		}
+// 	}
+// }
 
+/*
+    pattern 24:
+    0 1 0 1 0
+    0 1 0 1 0 
+    0 1 0 1 0 
+    0 1 0 1 0 
+    0 1 0 1 0 
+ */
+    public class pattern03 {
+    
+        public static void main(String[] args) {
+            
+            for(int i =1; i <= 5; i++) {
+
+                for(int j = 0; j < 5; j++) {
+
+                    System.out.print(((j) % 2) + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
     
