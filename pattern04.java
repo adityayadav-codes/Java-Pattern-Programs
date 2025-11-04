06d634684c1369f68917f911dfaa60f806114761
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-04 17:28:22 +0530

    patter 31

--- pattern04.java
+++ pattern04.java
@@ -4,5 +4,20 @@
     B C D E F
     C D E F G
     D E F G H 
-    E F G H I
+    E F G H i
  */
+
+ public class pattern04 {
+	
+	public static void main(String args [ ]) {
+		
+		for(int i = 0; i < 5; i++) {
+			
+			for(int j = 0; j < 5; j++) {
+				
+				System.out.print(((char)(i + j + 65 ) + " "));
+			}
+				System.out.println();
+		}
+	}
+}
