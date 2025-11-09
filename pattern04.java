92268f3ac61de33e67b0fcaaad1e3cd68466a338
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-09 22:31:55 +0530

    pattern 36

--- pattern04.java
+++ pattern04.java
@@ -99,19 +99,42 @@
         3 3 3 
         4 4 4 4
         5 5 5 5 5
-     */
+    //  */
 
-     public class pattern04 {
+    //  public class pattern04 {
      
-        public static void main(String[] args) {
+    //     public static void main(String[] args) {
             
-            for(int i =1; i <= 5; i++) {
+    //         for(int i =1; i <= 5; i++) {
 
-                for(int j =1; j <= i; j++){
+    //             for(int j =1; j <= i; j++){
 
-                    System.out.print(i + " ");
+    //                 System.out.print(i + " ");
+    //             }
+    //             System.out.println();
+    //         }
+    //     }
+    //  }
+
+    /*
+        pattern 36:
+        1
+        1 2
+        1 2 3
+        1 2 3 4
+        1 2 3 4 5
+     */
+    public class pattern04 {
+    
+        public static void main(String args []) {
+
+            for(int i = 1; i <= 5; i++) {
+
+                for(int j =1;j <= i; j++) {
+
+                    System.out.print(j + " ");
                 }
                 System.out.println();
             }
         }
-     }
+    }
