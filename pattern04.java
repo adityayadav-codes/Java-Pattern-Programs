6fef0b85b87a2bd59e97f99f18ddbdd1d41c6ca1
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-09 22:22:01 +0530

    pattern 33, 34 and 35

--- pattern04.java
+++ pattern04.java
@@ -20,7 +20,7 @@
 // 				System.out.println();
 // 		}
 // 	}
-// }
+//}
 /*
     pattern 32:
     A F K P U 
@@ -29,19 +29,89 @@
     D I N S X
     E J 0 T Y
  */
-    public class pattern04 {
+    // public class pattern04 {
 
-        public static void main(String args []) {
+    //     public static void main(String args []) {
 
-            int x;
-            for(int i = 0; i < 5; i++) {
+    //         int x;
+    //         for(int i = 0; i < 5; i++) {
 
-                x = i;
-                for(int j =1; j <= 5;j++) {
-                    x+=5;
-                    System.out.print((char)(x - 5 + 65) + " ");
+    //             x = i;
+    //             for(int j =1; j <= 5;j++) {
+    //                 x+=5;
+    //                 System.out.print((char)(x - 5 + 65) + " ");
+    //             }
+    //             System.out.println();
+    //         }
+    //     }
+    // }
+
+/*
+    pattern 33:
+    E J O T Y
+    D I N S X
+    C H M R W
+    B G L Q V
+    A F K P U
+// //  */
+// public class pattern04 {
+
+//     public static void main(String args []) {
+//         int x;
+//         for(int i =1; i <= 5; i++) {
+//             x = 5-i;
+//             for(int j =1;j <=5;j++) {
+//             System.out.print((char)(x + 65)+" ");
+//             x+=5;
+//             }
+//              System.out.println();
+//         }
+       
+//     }
+// }
+/*
+    pattern 34:
+    * 
+    * *
+    * * *
+    * * * * 
+    * * * * * 
+//  */
+//     public class pattern04 {
+    
+//        public static void main(String[] args) {
+        
+//             for(int i = 1; i <= 5; i++) {
+
+//                 for(int j =1; j <= i; j++) {
+                    
+//                     System.out.print("*"+" ");
+//                 }
+//                 System.out.println();
+//             }
+//         }
+//     }
+
+    
+    /* pattern 35:
+        1 
+        2 2 
+        3 3 3 
+        4 4 4 4
+        5 5 5 5 5
+     */
+
+     public class pattern04 {
+     
+        public static void main(String[] args) {
+            
+            for(int i =1; i <= 5; i++) {
+
+                for(int j =1; j <= i; j++){
+
+                    System.out.print(i + " ");
                 }
                 System.out.println();
             }
         }
-    }
+     }
