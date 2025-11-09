e279faef26300991165a5dcb61bdbaa76bdb91e4
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-09 22:58:53 +0530

    pattern 39

--- pattern04.java
+++ pattern04.java
@@ -168,15 +168,36 @@
     5 4 3 
     5 4 3 2
     5 4 3 2 1
+//  */
+// public class pattern04 {
+
+//     public static void main(String[] args) {
+        
+//         for(int i = 5; i >= 1; i--) {
+
+//             for(int j = 5; j>= i; j--) {
+
+//                 System.out.print(j + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+/*
+    pattern 39:
+    5
+    4 5
+    3 4 5
+    2 3 4 5
+    1 2 3 4 5 
  */
 public class pattern04 {
 
     public static void main(String[] args) {
-        
         for(int i = 5; i >= 1; i--) {
 
-            for(int j = 5; j>= i; j--) {
-
+            for(int j =i; j<=5; j++) {
                 System.out.print(j + " ");
             }
             System.out.println();
