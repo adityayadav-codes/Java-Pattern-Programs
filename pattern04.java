bc273cb17e8411f9d1995ac6f4105d403974e378
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-09 22:51:52 +0530

    pattern 38

--- pattern04.java
+++ pattern04.java
@@ -146,18 +146,40 @@
         3 3 3 
         2 2 2 2 
         1 1 1 1 1  
-     */
-    public class pattern04 {
+//      */
+//     public class pattern04 {
 	
-	public static void main(String args []) {
+// 	public static void main(String args []) {
 		
-		for(int i = 5; i >= 1; i--){
+// 		for(int i = 5; i >= 1; i--){
 			
-			for(int j =5; j >= i; j--){
+// 			for(int j =5; j >= i; j--){
 				
-				System.out.print(i + " ");
-			}
-				System.out.println();
-		}
-	}
+// 				System.out.print(i + " ");
+// 			}
+// 				System.out.println();
+// 		}
+// 	}
+// }
+/*
+    pattern 38:
+    5
+    5 4
+    5 4 3 
+    5 4 3 2
+    5 4 3 2 1
+ */
+public class pattern04 {
+
+    public static void main(String[] args) {
+        
+        for(int i = 5; i >= 1; i--) {
+
+            for(int j = 5; j>= i; j--) {
+
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
 }
